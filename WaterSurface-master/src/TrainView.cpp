9b67d6d7338e0a5ccb17281abc888e0f7cf018ce
jack6332 <b10815044@gapps.ntust.edu.tw>
2020-12-24 19:30:02 +0800

    fix

--- WaterSurface-master/src/TrainView.cpp
+++ WaterSurface-master/src/TrainView.cpp
@@ -685,21 +685,23 @@ void TrainView::draw()
 			 -0.5f,  0.5f, 0.0f,
 			  0.5f,  0.5f, 0.0f,
 			};
+
+			glGenVertexArrays(1, &particleVAO);
+			glBindVertexArray(particleVAO);
 			glGenBuffers(1, &billboard_vertex_buffer);
 			glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
 			// The VBO containing the positions and sizes of the particles
 			glGenBuffers(1, &particles_position_buffer);
 			glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
 			// Initialize with empty (NULL) buffer : it will be updated later, each frame.
 			glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
-
 			// The VBO containing the colors of the particles
 			glGenBuffers(1, &particles_color_buffer);
 			glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
 			// Initialize with empty (NULL) buffer : it will be updated later, each frame.
 			glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+
 			lastTime = tw->wave_t;
 		}
 		if (!this->tile) {
@@ -780,8 +782,8 @@ void TrainView::draw()
 				 1.0f, -1.0f,  1.0f
 			};
 			glGenVertexArrays(1, &skyboxVAO);
-			glGenBuffers(1, &skyboxVBO);
 			glBindVertexArray(skyboxVAO);
+			glGenBuffers(1, &skyboxVBO);
 			glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
 			glEnableVertexAttribArray(0);
@@ -817,8 +819,8 @@ void TrainView::draw()
 			};
 			// screen quad VAO
 			glGenVertexArrays(1, &quadVAO);
-			glGenBuffers(1, &quadVBO);
 			glBindVertexArray(quadVAO);
+			glGenBuffers(1, &quadVBO);
 			glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
 			glEnableVertexAttribArray(0);
@@ -867,8 +869,8 @@ void TrainView::draw()
 			};
 			// screen quad VAO
 			glGenVertexArrays(1, &quadVAO1);
-			glGenBuffers(1, &quadVBO1);
 			glBindVertexArray(quadVAO1);
+			glGenBuffers(1, &quadVBO1);
 			glBindBuffer(GL_ARRAY_BUFFER, quadVBO1);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
 			glEnableVertexAttribArray(0);
